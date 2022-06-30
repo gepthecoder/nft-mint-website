79b36cdf4f9ac792358c70b4137854fd3b31b1f5
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-06-30 11:25:20 +0200

    $0016: [App] [Style]: add style to app component - moving background + fonts

--- mint-nfts-website/src/App.js
+++ mint-nfts-website/src/App.js
@@ -1,5 +1,5 @@
 // style
-import './App.css';
+import "./App.css";
 
 // hooks
 import { useState } from 'react';
@@ -13,10 +13,17 @@ function App() {
   const [accounts, setAccounts] = useState([]);
 
   return (
-    <div className="App">
-      <NavBar accounts={accounts} setAccounts={setAccounts} />
-      <MainMint accounts={accounts} setAccounts={setAccounts} />
+    <div className="overlay">
+      <div className="App">
+        <NavBar accounts={accounts} setAccounts={setAccounts} />
+        <MainMint accounts={accounts} setAccounts={setAccounts} />
+      </div>
+      <div className="moving-background">
+
+      </div>
     </div>
+    
+   
   );
 }
 
